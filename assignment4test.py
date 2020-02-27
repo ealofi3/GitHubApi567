411a9fa4b8131ea8e940f493df7ebdf70cd98021
ealofi3 <57627548+ealofi3@users.noreply.github.com>
2020-02-27 03:06:23 -0500

    Update assignment4test.py

--- assignment4test.py
+++ assignment4test.py
@@ -1,9 +1,13 @@
-import unittest
-import requests
+import  unittest
 from assignment4 import getInfoApi
-class TestApi(unittest.TestCase):
-    def test_Api(self):
-        self.assertEqual(getInfoApi('ealofi3'), 'Right')
-       
+from unittest import mock
+
+class TestMockApi(unittest.TestCase):   
+    def test_mock_api(self):
+        getInfoApi = mock.Mock(return_value = "The user has 5 repos, names and commits as follows: /nName: GitHubApi567; Commits: 9 /nName: Triangle567; Commits: 9 /nName: project-810-A; Commits: 9 /nName: helloworld; Commits: 9 ; Commits: 9 /nName: assignment5; Commits: 9 ")
+        result = getInfoApi("ealofi3")
+        self.assertEqual(result, "The user has 5 repos, names and commits as follows: /nName: GitHubApi567; Commits: 9 /nName: Triangle567; Commits: 9 /nName: project-810-A; Commits: 9 /nName: helloworld; Commits: 9 ; Commits: 9 /nName: assignment5; Commits: 9 ")
+        
+    
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
